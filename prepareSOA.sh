e26820ce34afef57356c083341683165a3944e36
Anusky <anusky.pitrusky@gmail.com>
2015-09-25 17:14:13 +0200

    j

---
+++ prepareSOA.sh
@@ -0,0 +1,5 @@
+# INSTALLDIR tiene que apuntar al directorio donde este instalado el bochs 
+export INSTALLDIR ${HOME}/soa/mybochs
+
+export BXSHARE ${INSTALLDIR}/share/bochs
+export PATH ${INSTALLDIR}/bin:${PATH}
---
+++ zeos/user.c
@@ -0,0 +1,38 @@
+#include <libc.h>
+
+char buff[24];
+
+int pid;
+
+long inner(long n)
+{
+	int i;
+	long suma;
+	suma = 0;
+	for(i=0; i<n; i++) suma += i;
+	return suma;
+}
+
+long outer(long n)
+{
+	int i;
+	long acum;
+	acum = 0;
+	for(i=0;i<n; i++) acum += inner(i);
+	return acum;
+}
+
+int __attribute__ ((__section__(".text.main")))
+  main(void)
+{
+	long count,acum;
+	count = 75;
+	acum = 0;
+	acum = outer(count);
+    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
+     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+
+    
+  	while(1);
+	return 0;
+}
