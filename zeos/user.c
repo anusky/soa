45c4360d9a863eb9e9ca4402f8be4e36af2edf70
Anusky <anusky.pitrusky@gmail.com>
2015-09-28 16:29:51 +0200

    zeos preparado

--- zeos/user.c
+++ zeos/user.c
@@ -37,16 +37,10 @@ long outer(long n)
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
-	/*long count,acum;
-	count = 75;
-	acum = 0;
-	acum = outer(count);*/
-    
-
-    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
+	/* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-   add(2,2);
+   //add(2,2);
 
 
   	//while(1);
