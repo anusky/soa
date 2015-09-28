761b90fa2aca527356ba6c36e0fc1f11bba63b3f
Anusky <anusky.pitrusky@gmail.com>
2015-09-29 01:33:50 +0200

    prepareSOA

--- prepareSOA.sh
+++ prepareSOA.sh
@@ -1,5 +1,6 @@
 # INSTALLDIR tiene que apuntar al directorio donde este instalado el bochs 
-export INSTALLDIR ${HOME}/soa/mybochs
+#Add Bochs to path --> .bashrc
 
-export BXSHARE ${INSTALLDIR}/share/bochs
-export PATH ${INSTALLDIR}/bin:${PATH}
+export INSTALLDIR="$HOME/soa/mybochs"
+export BXSHARE="$INSTALLDIR/share/bochs"
+export PATH="$INSTALLDIR/bin:$PATH"
