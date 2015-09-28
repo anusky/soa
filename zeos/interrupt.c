49b6f4f493a681246747f8cfd930c743f15b97c6
Anusky <anusky.pitrusky@gmail.com>
2015-09-28 17:32:33 +0200

    cambios hasta system_call_handler

--- zeos/interrupt.c
+++ zeos/interrupt.c
@@ -83,6 +83,8 @@ void setIdt()
   set_handlers();
 
   /* ADD INITIALIZATION CODE FOR INTERRUPT VECTOR */
+  setInterruptHandler(33, keyboard_handler, 0);
+  setTrapHandler(0x80, system_call_handler, 3);
 
   set_idt_reg(&idtR);
 }
