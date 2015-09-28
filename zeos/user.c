a45e344047f00656e83cf535fc80a855e7b2906c
Anusky <anusky.pitrusky@gmail.com>
2015-09-28 15:12:21 +0200

    zeos renovado

--- zeos/user.c
+++ zeos/user.c
@@ -4,16 +4,16 @@ char buff[24];
 
 int pid;
 
-int add(int par1, int par2) {
+void add(int par1, int par2) {
 	/*
 	*	Declaro una local para no machacar el valor de eax después
 	*/
 	int ret;
 	__asm__ __volatile__("addl %%ebx, %%eax"
 							:"=a"(ret)
-							:"a"(par2), "b"(par2)
+							:"a"(par1), "b"(par2)
 		);
-	return ret;
+
 }
 
 long inner(long n)
@@ -46,9 +46,7 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    int par1, par2, suma;
-    par1 = 3, par2 = 6;
-    suma = add(par1, par2);
+   add(2,2);
 
 
   	//while(1);
