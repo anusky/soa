7668c0bebe32283f2ed43c99c7c42327e7175ce2
anusky <anusky.pitrusky@gmail.com>
2015-09-29 14:59:22 +0200

    añado el errno

--- zeos/user.c
+++ zeos/user.c
@@ -1,19 +1,24 @@
+#include <stdio.h>
 #include <libc.h>
+
 
 char buff[24];
 
 int pid;
 
-void add(int par1, int par2) {
-	/*
-	*	Declaro una local para no machacar el valor de eax después
-	*/
-	int ret;
-	__asm__ __volatile__("addl %%ebx, %%eax"
-	                     :"=a"(ret)
-	                     :"a"(par1), "b"(par2)
-	                    );
-
+int add(int par1, int par2) {
+int ret;
+	//=a le indicamos que quiero usar el registro a y que guardare alli 
+	//la suma de par1 y par2 
+	//"b"(par2) le indico que quiero usar el registro b, y que cargo en él, el contenido de par2
+	//"a"(par1) --> lo mismo que con b pero con el registro a y par1
+	__asm__ __volatile__(
+		"addl %%ebx, %%eax"
+		:"=a"(ret) /* output */
+		:"a"(par1), "b"(par2)  /* input */
+	);
+	
+	return ret;
 }
 
 long inner(long n)
@@ -21,7 +26,7 @@ long inner(long n)
 	int i;
 	long suma;
 	suma = 0;
-	for (i = 0; i < n; i++) suma += i;
+	for(i=0; i<n; i++) suma += i;
 	return suma;
 }
 
@@ -30,19 +35,27 @@ long outer(long n)
 	int i;
 	long acum;
 	acum = 0;
-	for (i = 0; i < n; i++) acum += inner(i);
+	for(i=0;i<n; i++) acum += inner(i);
 	return acum;
 }
 
 int __attribute__ ((__section__(".text.main")))
-main(void)
+  main(void)
 {
-	/* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
-	/* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
+/*	long count,acum;
+	count = 75;
+	acum = 0;
+	acum = outer(count);
+*/
+	
+	add(1, 2); //res 3
+	
+	
+    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
+     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-	//add(2,2);
-
-
-	//while(1);
+    
+  	while(1);
 	return 0;
 }
+
