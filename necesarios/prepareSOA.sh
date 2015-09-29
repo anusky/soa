1ba55c3a668428824ad3dd7747ef4f98c81ae5e9
Anusky <anusky.pitrusky@gmail.com>
2015-09-29 17:16:46 +0200

    errno

---
+++ necesarios/prepareSOA.sh
@@ -0,0 +1,5 @@
+# INSTALLDIR tiene que apuntar al directorio donde este instalado el bochs 
+setenv INSTALLDIR ${HOME}/Documents/mybochs
+
+setenv BXSHARE ${INSTALLDIR}/share/bochs
+setenv PATH ${INSTALLDIR}/bin:${PATH}
