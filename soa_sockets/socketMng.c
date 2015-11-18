7496a28cb26b5842f6fd10a9713823de578b935c
Anusky <anusky.pitrusky@gmail.com>
2015-11-18 23:03:27 +0100

    sockets 18-11

--- soa_sockets/socketMng.c
+++ soa_sockets/socketMng.c
@@ -16,7 +16,56 @@
 //
 
 int createServerSocket (int port) {
-	
+  //int socket(int domain, int type, int protocol);
+  /**
+   *  domain    :   AF_INET | PF_INET | PF_UNIX
+   *  types     :   SOCK_STREAM (REMOTO) | SOCK_DGRAM (LOCAL)
+   *  protocol  :   Dejar con 0 y el sistema elige el apropiado
+   *  
+   *  returns   :   Canal asociado si OK ; -1 si KO
+   */
+
+  //int bind();
+  /**
+   *  domain    :   AF_INET | PF_INET | PF_UNIX
+   *  types     :   SOCK_STREAM (REMOTO) | SOCK_DGRAM (LOCAL)
+   *  protocol  :   Dejar con 0 y el sistema elige el apropiado
+   *  
+   *  returns   :   Canal asociado si OK ; -1 si KO
+   */
+
+  //int listen(int canal, int backlog);
+  /**
+   *  canal       :   el que he creado con socket()
+   *  backlog     :   #peticiones pendientes que puede tener un servidor. 
+   *                  Si se supera el cliente recibe error en la peticion de conexion
+   *  
+   *  returns     :   0 - OK | -1 KO
+   */
+  
+  struct sockaddr_in socket_address;
+  int socket_fd;
+
+  socket_fd = socket(AF_INET, SOCK_STREAM, 0); //CANAL
+  
+  if(socket_fd != -1)  {
+    socket_address.sin_family_t = PF_INET;
+    socket_address.sin_addr = INADDR_ANY; //representa IP de la maquina donde ejecutamos el codigo
+    socket_address.sin_port = port; 
+
+
+    int socket_bind = bind(socket_fd, &(socket_address), sizeof(socket_address));
+
+    if(socket_bind < 0) {
+      //errno ¿?
+      return -1;
+    } else {
+      return listen(socket_fd, 3);
+    }
+    return -1;  
+  }
+  return socket_fd;
+
 }
 
 
@@ -28,7 +77,16 @@ int createServerSocket (int port) {
 int
 acceptNewConnections (int socket_fd)
 {
-
+  //int accept(int canal, struct sockaddr *direccion, socklen_t *tam_direccion);
+  /**
+   *  canal    :   canal que recibo por parametro
+   *  direccion     :   obiene la direccion del cliente que solicita la sireccion
+   *  tam_direccion  :   contiene el tamaño que ocupa la direccion
+   *  
+   *  returns   :   Canal para usar en la transmision si OK ; -1 si KO
+   */
+  struct sockaddr_in serv_addr;
+  int canal = accept(socket_fd, &(serv_addr), sizeof(serv_addr));
 }
 
 // Returns the socket virtual device that the client should use to access 
